e1c610190b11ccf4a88014154b0d8c7f24bac596
sdlqgjy <sdlqgjy@gmail.com>
2023-02-03 23:02:32 +0800

    测试vs2019并进行vs2019的基础配置，配置自定义模板

---
+++ test2_3/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<stdlib.h>
+
+//@file    :test
+//@author  :sdlqgjy
+//@data    :2023/2/3 22:53:13
+//@brief   :C语言的简单测试
+
+int main()
+{
+	printf("Hello world\n");
+	printf("你好世界\n");
+	system("pause");
+	return 0;
+}
+
